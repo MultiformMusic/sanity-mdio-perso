673364f4e9d9b4cf1dbca8d8db06568690c35eb9
mdio <michel.dio33@gmail.com>
2020-07-20 22:28:14 +0200

    musics + logos

--- schemas/schema.js
+++ schemas/schema.js
@@ -278,6 +278,40 @@ export default createSchema({
         }
       ]
 
+    },
+
+    // ***** MUSICS
+    {
+      name: 'musics',
+      title: 'Musics',
+      type: 'document',
+      fields: [
+        {
+          name: 'name',
+          title: 'Music Name',
+          type: 'string'
+        },
+        {
+          name: 'visibility',
+          title: 'Visibility',
+          type: 'boolean'
+        },
+        {
+          name: 'linkList',
+          title: 'Music Link List',
+          type: 'string'
+        },
+        {
+          name: 'linkBlock',
+          title: 'Music Link Block',
+          type: 'string'
+        },
+        {        
+          name: 'pistNumber',
+          title: 'Pist Number',
+          type: 'number'
+        }
+      ]
     }
 
   ])
